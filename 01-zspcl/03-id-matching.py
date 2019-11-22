d5baddcef1b3fab09c1550ff414eb5be363c311a
Godwin Kwan <46844102+godwinkwan@users.noreply.github.com>
2019-11-22 20:55:17 +0800

    Update 03-id-matching

--- 01-zspcl/03-id-matching.py
+++ 01-zspcl/03-id-matching.py
@@ -88,6 +88,8 @@ def id_match(files_path: str):
         excel_writer.save()
         excel_writer.close()
 
+        print('Done Sales.')
+
     # 匹配药店活动
     data_database = pandas.DataFrame(pandas.read_excel(os.path.join(files_path, extract_files[0]),
                                                        sheet_name='药店活动',
@@ -130,7 +132,7 @@ def id_match(files_path: str):
         excel_writer = pandas.ExcelWriter(os.path.join(files_path, extract_files[i]), engine='xlsxwriter')
         excel_book = openpyxl.load_workbook(excel_writer.path)
         excel_writer.book = excel_book
-        # excel_writer.sheets = dict((worksheet.title, worksheet) for worksheet in excel_book.worksheets)
+        excel_writer.sheets = dict((worksheet.title, worksheet) for worksheet in excel_book.worksheets)
         data_unit.to_excel(
             excel_writer, sheet_name=sheet_name, encoding='utf-8', index=False, header=True, startrow=3
         )
